a5ea463a181fa8994483f3b509e7c48d4e238cd2
palasebi <pala.sebi@yahoo.com>
2026-01-02 18:44:24 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -148,3 +148,18 @@ async def checker():
 # --- Inițializare ---
 bosses = load_bosses()
 client.run(TOKEN)
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
+
